094673a78b58a33f7945b0f0efc7c0a43a4bbcab
Guilherme Mena <guilherme.atu@gmail.com>
2024-03-09 17:12:46 -0300

    add posts and tags schema

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+import { pgTable, uuid, text, timestamp, primaryKey } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
   id: uuid('id').defaultRandom().notNull().primaryKey(),
@@ -11,3 +12,76 @@ export const users = pgTable('users', {
     .notNull()
     .defaultNow(),
 })
+
+export const posts = pgTable('posts', {
+  id: uuid('id').defaultRandom().notNull().primaryKey(),
+  title: text('title').notNull(),
+  description: text('content'),
+  url: text('url').notNull(),
+  image_url: text('image_url'),
+  user_id: uuid('user_id')
+    .notNull()
+    .references(() => users.id),
+  created_at: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+  updated_at: timestamp('updated_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
+
+export const postRelations = relations(posts, ({ one, many }) => ({
+  user: one(users, {
+    fields: [posts.user_id],
+    references: [users.id],
+  }),
+  postTags: many(postOnTags),
+}))
+
+export const tags = pgTable('tags', {
+  id: uuid('id').defaultRandom().notNull().primaryKey(),
+  name: text('name').notNull(),
+  user_id: uuid('user_id')
+    .notNull()
+    .references(() => users.id),
+  created_at: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+  updated_at: timestamp('updated_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
+
+export const tagRelations = relations(tags, ({ one, many }) => ({
+  user: one(users, {
+    fields: [tags.user_id],
+    references: [users.id],
+  }),
+  posts: many(postOnTags),
+}))
+
+export const postOnTags = pgTable(
+  'post_tags',
+  {
+    post_id: uuid('post_id')
+      .notNull()
+      .references(() => posts.id),
+    tag_id: uuid('tag_id')
+      .notNull()
+      .references(() => tags.id),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.post_id, t.tag_id] }),
+  }),
+)
+
+export const postOnTagsRelations = relations(postOnTags, ({ one }) => ({
+  post: one(posts, {
+    fields: [postOnTags.post_id],
+    references: [posts.id],
+  }),
+  tag: one(tags, {
+    fields: [postOnTags.tag_id],
+    references: [tags.id],
+  }),
+}))
